1366e30139f82e4c4e78875a5674082caaff340c
roman-yagodin <roman.yagodin@gmail.com>
2020-10-27 00:02:46 +0400

    Rotate cards on ui_left/ui_right actions

--- Game.cs
+++ Game.cs
@@ -3,6 +3,10 @@ using System;
 
 public class Game : Spatial
 {
+	protected Camera Camera => GetNode<Camera> (nameof (Camera));
+
+	protected Spatial Board => GetNode<Spatial> (nameof (Board));
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -13,5 +17,15 @@ public class Game : Spatial
 	public override void _Process(float delta)
 	{
 
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (inputEvent.IsActionPressed("ui_left")) {
+			Board.Rotate (new Vector3 (0, 1, 0), (float) Math.PI / 16);
+		}
+		else if (inputEvent.IsActionPressed("ui_right")) {
+			Board.Rotate (new Vector3 (0, 1, 0), -(float) Math.PI / 16);
+		}
 	}
 }
