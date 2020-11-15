ea2900ceac8123b94a23cdd88e6a066ecf736057
roman-yagodin <roman.yagodin@gmail.com>
2020-11-15 11:51:12 +0400

    Add two new cards

--- Models/CardDatabase.cs
+++ Models/CardDatabase.cs
@@ -4,27 +4,29 @@ using System.Collections.Generic;
 
 public static class CardDatabase
 {
-    public static IDictionary<string, ICard> Cards;
+	public static IDictionary<string, ICard> Cards;
 
-    static CardDatabase ()
-    {
-        Cards = new Dictionary<string, ICard> ();
+	static CardDatabase ()
+	{
+		Cards = new Dictionary<string, ICard> ();
 
-        AddCard ("green_drake", 0, 1, 2, 3);
-        AddCard ("blue_drake", 1, 2, 3, 4);
-        AddCard ("red_drake", 2, 3, 4, 5);
-    }
+		AddCard ("green_drake", 0, 1, 2, 3);
+		AddCard ("blue_drake", 1, 2, 3, 4);
+		AddCard ("red_drake", 2, 3, 4, 5);
+		AddCard ("gray_drake", 3, 4, 5, 6);
+		AddCard ("brown_drake", 4, 5, 6, 7);
+	}
 
-    static void AddCard (string cardName, params int [] values)
-    {
-        Cards.Add (cardName, new Card (cardName, values));
-    }
+	static void AddCard (string cardName, params int [] values)
+	{
+		Cards.Add (cardName, new Card (cardName, values));
+	}
 
-    public static ICard GetCard (string cardName)
-    {
-        if (cardName != null && Cards.TryGetValue (cardName, out ICard card)) {
-            return ((Card) card).Clone ();
-        }
-        return null;
-    }
+	public static ICard GetCard (string cardName)
+	{
+		if (cardName != null && Cards.TryGetValue (cardName, out ICard card)) {
+			return ((Card) card).Clone ();
+		}
+		return null;
+	}
 }
--- Models/CardSamples.cs
+++ Models/CardSamples.cs
@@ -7,32 +7,40 @@ using System.Collections.Generic;
 /// </summary>
 public static class CardSamples
 {
-    public static IDictionary<string, ICard> Cards;
+	public static IDictionary<string, ICard> Cards;
 
-    static CardSamples ()
-    {
-        Cards = new Dictionary<string, ICard> ();
-        
-        var card1 = CardDatabase.GetCard ("green_drake");
-        Cards.Add ("green_drake_01", card1);
+	static CardSamples ()
+	{
+		Cards = new Dictionary<string, ICard> ();
+		
+		var card1 = CardDatabase.GetCard ("green_drake");
+		Cards.Add ("green_drake_01", card1);
 
-        var card2 = CardDatabase.GetCard ("blue_drake");
-        card2.Owner = CardOwner.Blue;
-        Cards.Add ("blue_drake_01", card2);
+		var card2 = CardDatabase.GetCard ("blue_drake");
+		card2.Owner = CardOwner.Blue;
+		Cards.Add ("blue_drake_01", card2);
 
-        var card3 = CardDatabase.GetCard ("red_drake");
-        card3.Owner = CardOwner.Red;
-        Cards.Add ("red_drake_01", card3);
+		var card3 = CardDatabase.GetCard ("red_drake");
+		card3.Owner = CardOwner.Red;
+		Cards.Add ("red_drake_01", card3);
+		
+		var card4 = CardDatabase.GetCard ("gray_drake");
+		card4.Owner = CardOwner.Neutral;
+		Cards.Add ("gray_drake_01", card4);
+		
+		var card5 = CardDatabase.GetCard ("brown_drake");
+		card5.Owner = CardOwner.Red;
+		Cards.Add ("brown_drake_01", card5);
+		
+		var blank = new Card { IsBlank = true };
+		Cards.Add ("blank", blank);
+	}
 
-        var card4 = new Card { IsBlank = true };
-        Cards.Add ("blank", card4);
-    }
-
-    public static ICard GetCard (string cardSampleName)
-    {
-        if (cardSampleName != null && Cards.TryGetValue (cardSampleName, out ICard card)) {
-            return ((Card) card).Clone ();
-        }
-        return null;
-    }
+	public static ICard GetCard (string cardSampleName)
+	{
+		if (cardSampleName != null && Cards.TryGetValue (cardSampleName, out ICard card)) {
+			return ((Card) card).Clone ();
+		}
+		return null;
+	}
 }
