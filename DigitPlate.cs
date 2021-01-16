5b474c3cd05f9a719d95ceaa6464941da8f03ba2
roman-yagodin <roman.yagodin@gmail.com>
2021-01-16 23:56:19 +0300

    Make DigitPlate a "tool"

--- DigitPlate.cs
+++ DigitPlate.cs
@@ -1,9 +1,9 @@
 using Godot;
 using System;
 
+[Tool]
 public class DigitPlate : Spatial
 {
-
 	protected Sprite3D Sprite3D => GetNode<Sprite3D> (nameof (Sprite3D));
 
 	int _digit;
@@ -13,27 +13,16 @@ public class DigitPlate : Spatial
 		get { return _digit; }
 		set {
 			_digit = value;
-			LoadTexture ();
+			Bind (_digit);
 		}
 	}
 
-	void LoadTexture ()
+	void Bind (int digit)
 	{
+		if (digit < 0 || digit > 10) {
+			return;
+		}
+		
 		Sprite3D.Texture = GD.Load<StreamTexture> ($"res://textures/digits/d{_digit}.png");
 	}
-
-	// replace base class exported prop with non-exported
-	//public new Texture Texture { get; set; }
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		LoadTexture ();
-	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
