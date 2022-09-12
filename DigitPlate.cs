d62712db9e68d2f415a853dca7c1c22bfd353ac0
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-09-12 17:04:11 -0500

    Cleanup DigitPlate.cs

--- DigitPlate.cs
+++ DigitPlate.cs
@@ -5,7 +5,7 @@ public class DigitPlate : Spatial
 {
 	protected Sprite3D Sprite3D => GetNode<Sprite3D> (nameof (Sprite3D));
 
-	int _digit;
+	private int _digit;
 
 	[Export]
 	public int Digit {
@@ -16,11 +16,10 @@ public class DigitPlate : Spatial
 		}
 	}
 
-	void Bind (int digit)
+	private void Bind (int digit)
 	{
-		if (digit < 0 || digit > 10) {
+		if (digit < 0 || digit > 10)
 			return;
-		}
 		
 		Sprite3D.Texture = GD.Load<StreamTexture> ($"res://textures/digits/d{_digit}.png");
 	}
