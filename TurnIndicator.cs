f2f3c03369066358b92c51ac21159ee47032d93e
roman-yagodin <roman.yagodin@gmail.com>
2022-10-13 02:29:54 +0300

    Proper tetrahedron mesh for indicator

--- TurnIndicator.cs
+++ TurnIndicator.cs
@@ -20,37 +20,33 @@ public class TurnIndicator : MeshInstance
             }
         }
     }
+    
+    public TurnIndicator()
+    {
+        //var mesh = CreateMesh();
+        //ResourceSaver.Save("res://meshes/indicator.tres", mesh, ResourceSaver.SaverFlags.Compress);
+    }
 
     ArrayMesh CreateMesh()
     {
+        // from https://en.wikipedia.org/wiki/Tetrahedron
+        var a = new Vector3(0.943f, 0f, -0.333f);
+        var b = new Vector3(-0.471f, 0.816f, -0.333f);
+        var c = new Vector3(-0.471f, -0.816f, -0.333f);
+        var d = new Vector3(0f, 0f, 1f);
+
         var st = new SurfaceTool();
         st.Begin(Mesh.PrimitiveType.TriangleStrip);
-        st.AddColor(Color.ColorN("yellow"));
         
-        // TODO: probably this is not tetrahedron!
-        var a = new Vector3(0f, 1f, 0f);
-        var b = new Vector3(-0.86f, -0.5f, -0.5f);
-        var c = new Vector3(0.86f, -0.5f, -0.5f);
-        var d = new Vector3(0f, -0.5f, 1f);
-        
-        st.AddColor(Color.ColorN("yellow"));
-        st.AddVertex(a);
-        st.AddVertex(c);
-        st.AddVertex(b);
-        
-        st.AddColor(Color.ColorN("red"));
-        st.AddVertex(d);
-        st.AddVertex(a);
-
-        st.AddColor(Color.ColorN("blue"));
-        st.AddVertex(c);
-        st.AddVertex(d);
-
-        st.AddColor(Color.ColorN("green"));
-        st.AddVertex(b);
-        st.AddVertex(c);
+        st.AddColor(Color.Color8(255, 255, 255)); st.AddVertex(a);
+        st.AddColor(Color.Color8(255, 0, 0)); st.AddVertex(b);
+        st.AddColor(Color.Color8(0, 255, 0)); st.AddVertex(c);
+        st.AddColor(Color.Color8(0, 0, 255)); st.AddVertex(d);
+        st.AddColor(Color.Color8(255, 255, 255)); st.AddVertex(a);
+        st.AddColor(Color.Color8(255, 0, 0)); st.AddVertex(b);
 
         st.GenerateNormals();
+        st.GenerateTangents();
 
         return st.Commit();
     }
